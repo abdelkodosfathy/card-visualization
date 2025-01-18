54d6b7462ccbd6cb4f068d96f61b77459ca21b53
abdelkodosfathy <abdelkodosfathy@gmail.com>
2025-01-18 02:37:31 +0200

    rebuild

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"card-visualization",
+  base:"/card-visualization/",
   plugins: [react()],
   resolve: {
     alias: {
